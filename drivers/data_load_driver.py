876ab6d46a6031a505ef71ba7b1cffbe4d33ec84
chjort <christoffer.hjort1995@gmail.com>
2020-10-11 16:52:45 +0200

    Implementing dataloader for oneshot detection

---
+++ drivers/data_load_driver.py
@@ -0,0 +1,51 @@
+import torch
+from torch.utils.data import DataLoader, DistributedSampler
+
+import util.misc as utils
+from datasets.coco import CocoDetection, make_coco_transforms
+
+# %%
+BATCH_SIZE = 4
+NUM_WORKERS = 2
+distributed = False
+
+COCO_PATH = "/datadrive/crr/datasets/coco"
+
+dataset_train = CocoDetection(COCO_PATH + "/train2017",
+                              COCO_PATH + "/annotations/instances_train2017.json",
+                              transforms=make_coco_transforms("train"), return_masks=False)
+dataset_val = CocoDetection(COCO_PATH + "/val2017",
+                            COCO_PATH + "/annotations/instances_val2017.json",
+                            transforms=make_coco_transforms("val"), return_masks=False)
+
+it = iter(dataset_train)
+b = next(it)
+b[0].shape
+b[1].keys()
+
+# %%
+if distributed:
+    sampler_train = DistributedSampler(dataset_train)
+    sampler_val = DistributedSampler(dataset_val, shuffle=False)
+else:
+    sampler_train = torch.utils.data.RandomSampler(dataset_train)
+    sampler_val = torch.utils.data.SequentialSampler(dataset_val)
+
+batch_sampler_train = torch.utils.data.BatchSampler(
+    sampler_train, BATCH_SIZE, drop_last=True)
+
+data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,
+                               collate_fn=utils.collate_fn, num_workers=NUM_WORKERS)
+data_loader_val = DataLoader(dataset_val, BATCH_SIZE, sampler=sampler_val,
+                             drop_last=False, collate_fn=utils.collate_fn, num_workers=NUM_WORKERS)
+
+# %%
+train_it = iter(data_loader_train)
+val_it = iter(data_loader_val)
+
+# %%
+x, y = next(train_it)
+
+# %% sample 0
+x.tensors[0].shape
+y[0]
---
+++ drivers/instre_driver.py
@@ -0,0 +1,34 @@
+import numpy as np
+import os
+import glob
+from PIL import Image
+
+INSTRE_PATH = "/datadrive/crr/datasets/instre"
+TRAIN_PATH = INSTRE_PATH + "/INSTRE-S-TRAIN"
+TEST_PATH = INSTRE_PATH + "/INSTRE-S-TEST"
+
+
+def read_instre_box(box_file):
+    """
+    Returns box with format [x0, y0, w, h]
+    """
+
+    with open(box_file, "r") as f:
+        box = f.read().strip("\n").split(" ")
+        box = [int(c) for c in box]
+    return box
+
+
+# %%
+class_dirs = glob.glob(os.path.join(TRAIN_PATH, "*/"))
+
+c0 = class_dirs[0]
+sample0 = "002"
+
+c0_s0 = glob.glob(os.path.join(c0, sample0 + ".*"))
+
+s0_img = np.asarray(Image.open(c0_s0[1]))
+s0_box = read_instre_box(c0_s0[0])
+
+s0_img
+s0_box
